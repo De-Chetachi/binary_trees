8a8fab0e225844322e310bb76874e4a6c9191aa9
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-10-31 22:43:10 +0100

    ok

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,29 @@
 #include "binary_trees.h"
+
+/**
+* binary_tree_balance - checks the balance factor of a binary tree
+* @tree: root nodde
+* Return: balance factor
+*/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int height_left, height_right;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		height_left = 1 + binary_tree_balance(tree->left);
+	else
+		 height_left = 0;
+	if (tree->right)
+		height_right = 1 + binary_tree_balance(tree->right);
+	else
+		height_right = 0;
+
+	return (height_left - height_right);
+}
+
 /**
 * binary_tree_is_perfect - checks if a binary tree is perfect
 * @tree: root node
@@ -13,7 +38,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (binary_tree_balance(tree) != 0)
-		return (0)
+		return (0);
 
 	if (!tree->left && !tree->right)
 	{
@@ -23,8 +48,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		perfect = 1;
 	}
-	perfect = perfect * binary_tree_is_full(tree->left);
-	perfect = perfect * binary_tree_is_full(tree->right);
+	perfect = perfect * binary_tree_is_perfect(tree->left);
+	perfect = perfect * binary_tree_is_perfect(tree->right);
 
 	return (perfect);
 }
