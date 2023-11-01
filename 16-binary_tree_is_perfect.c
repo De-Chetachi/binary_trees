adb5a2d2d81067fa15119be00974862576e99689
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-01 20:26:52 +0100

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,21 +1,23 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_nodes - counts nodes with at least one child in a binary tree
+* binary_tree_fullnodes - counts full nodes with at least one child in a binary tree
 * @tree: pointer to the node whose leaves is to be measured
 * Return: leaves number
 */
 
-size_t binary_tree_nodes(const binary_tree_t *tree)
+size_t binary_tree_fullnodes(const binary_tree_t *tree)
 {
 	size_t nodes = 0;
 
 	if (!tree)
 		return (nodes);
-	if (tree->left || tree->right)
+	if (!tree->left && !tree->right)
+		nodes = 1;
+	if (tree->left && tree->right)
 	{
-		nodes = 1 + binary_tree_nodes(tree->left);
-		nodes = nodes + binary_tree_nodes(tree->right);
+		nodes = 1 + binary_tree_fullnodes(tree->left);
+		nodes = nodes + binary_tree_fullnodes(tree->right);
 	}
 	return (nodes);
 }
@@ -32,7 +34,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (binary_tree_nodes(tree->left) == binary_tree_nodes(tree->right))
+	if (binary_tree_fullnodes(tree->left) == binary_tree_fullnodes(tree->right))
 	{
 		/*l_full = binary_tree_is_full(tree->left);*/
 		/*r_full = binary_tree_is_full(tree->right);*/
