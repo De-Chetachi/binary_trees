8971a7882758e8e7efe420f523d9f80d711d3638
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-01 21:11:11 +0100

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+* binary_tree_height_b - traverse a tree preorder
+* @tree: root nodde
+* Return: void
+*/
+
+int binary_tree_height_b(const binary_tree_t *tree)
+{
+	int height_left = 1, height_right = 1;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		height_left = height_left + binary_tree_height_b(tree->left);
+
+	if (tree->right)
+		height_right = height_right + binary_tree_height_b(tree->right);
+
+	return (height_left > height_right ? height_left : height_right);
+}
+
+/**
 * binary_tree_balance - checks the balance factor of a binary tree
 * @tree: root nodde
 * Return: balance factor
@@ -12,14 +33,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (tree->left)
-		height_left = 1 + binary_tree_balance(tree->left);
-	else
-		 height_left = 0;
-	if (tree->right)
-		height_right = 1 + binary_tree_balance(tree->right);
-	else
-		height_right = 0;
+	height_left = binary_tree_height_b(tree->left);
+
+	height_right = binary_tree_height_b(tree->right);
 
 	return (height_left - height_right);
 }
